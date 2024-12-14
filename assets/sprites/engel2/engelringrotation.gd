40a09ef4c7dcc62906a9648073c64c22685f0b42
Seal <88384688+Seal187@users.noreply.github.com>
2024-12-15 00:33:40 +0100

    Code durch Kommentarentfernung verbessert
    und 1x halluzination entfernt

--- assets/sprites/engel2/engelringrotation.gd
+++ assets/sprites/engel2/engelringrotation.gd
@@ -1,28 +1,21 @@
 extends MeshInstance3D
 
-var current_axis = Vector3(1, 0, 0)  # Starting axis
-var target_axis = Vector3(1, 0, 0)  # Axis to blend towards
-var blend_speed = 8.0  # Speed of blending between axes (higher is more chaotic)
-var rot_speed = 80
+var current_axis = Vector3(1, 0, 0)
+var target_axis = Vector3(1, 0, 0) 
+var lerpspeed = 8.0
+var rotationspeed = 80
 
 func _ready():
-	randomize()  # Seed randomness
+	randomize()
 
 func _process(delta):
-	# Gradually blend towards the target axis for smooth swinging
-	current_axis = current_axis.lerp(target_axis, blend_speed * delta).normalized()
+	# lerp garantiert
+	current_axis = current_axis.lerp(target_axis, lerpspeed * delta).normalized()
+	rotate(current_axis, deg_to_rad(rotationspeed) * delta)
 
-	# Rotate smoothly around the current axis
-	var rotation_angle = deg_to_rad(rot_speed) * delta  # 60 degrees per second
-	rotate(current_axis, rotation_angle)
-
-	# Occasionally pick a new random target axis
-	if randf() < 0.05:  # Small probability of switching target axis per frame
-		_choose_new_target_axis()
-
-func _choose_new_target_axis():
-	# Generate a completely random axis
-	target_axis = Vector3(
+	# Selten neue Axis
+	if randf() < 0.05:  
+		target_axis = Vector3(
 		randf_range(-1, 1),
 		randf_range(-1, 1),
 		randf_range(-1, 1)
