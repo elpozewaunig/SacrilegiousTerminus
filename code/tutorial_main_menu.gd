fdfae4e382f9bed4a55cff0b5263934cdacf6abe
Elias Pozewaunig <elias.pozewaunig@aau.at>
2025-10-20 20:54:09 +0200

    Make tutorial exit button grab focus in main menu

--- code/tutorial_main_menu.gd
+++ code/tutorial_main_menu.gd
@@ -1,5 +1,10 @@
 extends CanvasLayer
 
+@onready var exit_btn: Button = $"%Button"
+
+func _ready() -> void:
+	exit_btn.grab_focus()
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause") or Input.is_action_just_pressed("input-next"):
 		_on_back_to_menu_btn_pressed()
